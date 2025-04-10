8fa0950e8e6e55022ed83bc89a0e3fc67b6fd587
xzudev <helixzu@protonmail.com>
2025-04-10 21:16:44 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const WebSocket = require('ws');
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const rooms = {};
+
+wss.on('connection', (ws) => {
+  let room = null;
+
+  ws.on('message', (data) => {
+    const message = JSON.parse(data);
+
+    if (message.type === 'join') {
+      room = message.url;
+      rooms[room] = rooms[room] || new Set();
+      rooms[room].add(ws);
+      return;
+    }
+
+    if (message.type === 'chat' && room) {
+      for (const client of rooms[room]) {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify({
+            user: message.user || 'Anonymous',
+            text: message.text
+          }));
+        }
+      }
+    }
+  });
+
+  ws.on('close', () => {
+    if (room && rooms[room]) {
+      rooms[room].delete(ws);
+      if (rooms[room].size === 0) delete rooms[room];
+    }
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('WebSocket server running');
+});
